8d30f10f5561d0a11de923789019c069c57f7984
diegosfc100 <diego.sfc100@hotmail.com>
2021-05-14 16:50:08 -0300

    add resumo SELECT

---
+++ 01.SELECT.sql
@@ -0,0 +1,15 @@
+-- SELECT 
+-- a ideia básica do SELECT é selecionar uma ou mais colunas(ou todas) de uma tabela.
+
+-- sintaxe
+SELECT coluna1, coluna2
+FROM tabela;
+
+-- selecionando todas colunas.
+SELECT *
+FROM tabela;
+
+
+-- selecionanando nome e sobrenome de todos clientes cadastrados no sistema.
+SELECT firstName, lastName
+FROM person.Person;
